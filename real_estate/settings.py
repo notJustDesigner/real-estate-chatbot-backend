aa806ef67d3b64453a6329a415332215286be3f4
notJustDesigner <yugandharpatil049@gmail.com>
2025-11-30 21:34:17 +0530

    changes

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    '*.onrender.com',
+    'real-estate-chatbot-backend-bdex.onrender.com',
+    'real-estate-bot-4m1t.onrender.com',
 ]
 
 
