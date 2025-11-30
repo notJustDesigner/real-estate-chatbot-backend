dcabc485f47cdd6629f15876dafdc5e2e491a0e7
notJustDesigner <yugandharpatil049@gmail.com>
2025-11-30 19:54:01 +0530

    render url added

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -60,6 +60,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",  # Vite default port
     "http://127.0.0.1:5173",
+    "https://real-estate-chatbot-backend-bdex.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
